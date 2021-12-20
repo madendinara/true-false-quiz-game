80dad44e445faf466bc77ae9038febe4f1aaf69c
Dinara Zimanova <madendinara@gmail.com>
2021-12-20 15:28:59 -0500

    Initial Commit

---
+++ true-false-quiz-game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  true-false-quiz-game
+//
+//  Created by Динара Зиманова on 12/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
